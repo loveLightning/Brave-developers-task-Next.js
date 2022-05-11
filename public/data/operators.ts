9fd96fff0cd6585fda0bfc92a712a2e513ff3c91
zaycevnet20 <zena81533@gmail.com>
2022-05-11 09:16:20 +0300

    fix code

--- public/data/operators.ts
+++ public/data/operators.ts
@@ -2,17 +2,17 @@ export const operators: Array<{id: number, operator: string, img: string}> = [
     {
         id: 1,
         operator: 'МТС',
-        img: '/../public/img/mts.jpg',
+        img: '/img/mts.jpg',
     },
     {
         id: 2,
         operator: 'Мегафон',
-        img: '/../public/img/megafon.png',
+        img: '/img/megafon.png',
     },
     {
         id: 3,
         operator: 'Билайн',
-        img: '/../public/img/beeline.jpg',
+        img: '/img/beeline.jpg',
     },
     
 ]
